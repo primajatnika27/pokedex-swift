c879f5d0898ae5afa7e58ea3da53dd1701091c2a
prima jatnika <primajatnika271995@gmail.com>
2024-05-13 10:24:37 +0700

    Initial Commit

---
+++ PokeCleanArch/PokeCleanArchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokeCleanArchApp.swift
+//  PokeCleanArch
+//
+//  Created by user on 13/05/24.
+//
+
+import SwiftUI
+
+@main
+struct PokeCleanArchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
